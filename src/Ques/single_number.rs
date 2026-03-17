498ea8c90009b4d55de2ebea414debf6d9f308d0
Roodius-hub <osmansaifi30@gmail.com>
2026-03-17 17:06:41 +0000

    xor

--- src/Ques/single_number.rs
+++ src/Ques/single_number.rs
@@ -2,7 +2,7 @@
 Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
 You must implement a solution with a linear runtime complexity and use only constant extra space.
 */
-use std::collections::HashSet;
+use std::{collections::HashSet, vec};
 pub fn single_number(nums:Vec<i32>) -> i32 {
     let mut set:HashSet<i32> = HashSet::<i32>::new();
 
@@ -16,9 +16,21 @@ pub fn single_number(nums:Vec<i32>) -> i32 {
     *set.iter().next().unwrap()
 }
     
+pub fn single_number_using_xor(nums:Vec<i32>) -> i32 {
+    let mut unique_number:i32 = 0;
+    for num in nums {
+        unique_number ^= num;
+    }
+    return unique_number
+}
 
 fn main(){
     let nums:Vec<i32> = vec![2,2,1];
-    let ans = single_number(nums);
+    let ans = single_number(nums.clone());
     println!(" nums is: {}", ans);
+
+    let mut nums2:Vec<i32> = vec![];
+    nums2 = nums.clone();
+    let ans2 = single_number_using_xor(nums2);
+    println!("xor: {}", ans2);
 }
