526ea2373667f4ba3ba1802eb21f6e7b4305bd12
Roodius-hub <osmansaifi30@gmail.com>
2026-05-20 05:21:47 +0000

    max_sum

--- src/Ques/Maximum_Subarray.rs
+++ src/Ques/Maximum_Subarray.rs
@@ -1,9 +1,22 @@
-pub fn max_sub_array(nums: Vec<i32>) -> i32 {
+use std::cmp::max;
 
+pub fn max_sub_array(nums: Vec<i32>) -> i32 {
+    let mut currentSum = 0;
+    let mut max_sum = std::i32::MIN;
+
+    for val in nums {
+        currentSum  += val;
+        max_sum = max(currentSum, max_sum);
+        if currentSum < 0 {
+            currentSum = 0;
+        }
+    }
+    max_sum
 }
 
 fn main() {
     let nums:Vec<i32> = vec![-2,1,-3,4,-1,2,1,-5,4];
     
-    max_sub_array(nums);
+    let ans = max_sub_array(nums);
+    println!("{}", ans);
 }
