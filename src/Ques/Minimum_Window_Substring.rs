77adb0c1a65a5638b2972b54d50486c107b9690c
Roodius-hub <osmansaifi30@gmail.com>
2026-05-27 22:42:03 -0700

    new Question

--- src/Ques/Minimum_Window_Substring.rs
+++ src/Ques/Minimum_Window_Substring.rs
@@ -7,14 +7,15 @@ use std::collections::HashMap;
 
 pub fn min_window(s: String, t: String) -> String {
     let mut need: HashMap<char, i32> = HashMap::new();
+    
     for ch in t.chars() {
         need.entry(ch).and_modify(|c| *c += 1).or_insert(1);
     }
 
     let mut window:HashMap<char, i32> = HashMap::new();
     let left = 0;
-    let matched:bool;
-    
+    let matched:bool;   
+
 
     "stuibg".to_string()        
 }
