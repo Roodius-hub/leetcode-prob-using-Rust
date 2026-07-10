1ebe7fd2dd2bc88890136f5c3f6a288b9f6095e3
Roodius-hub <osmansaifi30@gmail.com>
2026-07-10 07:42:01 -0700

    new

--- src/Ques/Find theIndexoftheFirstOccurrenceinaString.rs
+++ src/Ques/Find theIndexoftheFirstOccurrenceinaString.rs
@@ -4,6 +4,15 @@ pub fn str_str(haystack: String, needle: String) -> i32 {
     
     let n = haystack.len();
     let m = needle.len();
+
+    if n <= 0 {
+        return -1;
+    }
+
+    if m <=0 {
+        return -1;
+    }
+
     let mut ans:i32 = -1;
     for i in 0..(n-m) {
         if haystack[i..i + m] == needle {
