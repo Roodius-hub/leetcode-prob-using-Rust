367ff8090642a092e15dc8fd8b141e38167f83c9
Roodius-hub <osmansaifi30@gmail.com>
2026-07-20 23:14:39 -0700

    217

--- src/company/217.rs
+++ src/company/217.rs
@@ -1,9 +1,21 @@
 // contains duplicates
+use std::collections::HashMap;
 
 pub fn contains_duplicate(nums: Vec<i32>) -> bool {
-       
-}
+    let mut map = HashMap::new(); 
+
+    for val in nums {
+        let count = map.entry(val).or_insert(0);
+        *count += 1;
+        if *count > 1{
+            return true;
+        }
+    }
+    false
+}   
 
 fn main() {
-    
+    let nums = vec![1,2,3,1];
+    let ans  = contains_duplicate(nums);
+    println!("{}", ans);
 }
