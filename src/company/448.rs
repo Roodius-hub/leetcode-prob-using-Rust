e2194987c13285085bb7d09c2095f166ff1a8b9d
Roodius-hub <osmansaifi30@gmail.com>
2026-08-05 03:10:25 -0700

    448

--- src/company/448.rs
+++ src/company/448.rs
@@ -1,8 +1,26 @@
+use std::path::absolute;
 
 
 
-pub fn find_disappeared_numbers(nums: Vec<i32>) -> Vec<i32> {
+
+pub fn find_disappeared_numbers(mut nums: Vec<i32>) -> Vec<i32> {
     let n = nums.len();
+    let mut ans = Vec::new();
+
+    for i in 0..n {
+        let index = nums[i].abs() as usize -1;
+        if nums[index] > 0 {
+            nums[index] -= nums[index]
+        }
+    }
+    for i  in 0..n {
+        if nums[i] > 0 {
+            ans.push(i as i32 +1);
+        }
+    }
+    // println!("{:?}", nums);
+        
+    ans
 }
 
 
