c738ed34b3abdc18f95a13a872ca6181cb143219
Roodius-hub <osmansaifi30@gmail.com>
2026-08-25 02:44:46 -0700

    minimum window substring

--- src/company/76.rs
+++ src/company/76.rs
@@ -1,34 +1,77 @@
 use std::collections::HashMap;
 
 
-pub fn min_window(s:String, t:String) -> String {
-    let mut mapt:HashMap<char, i32>  = HashMap::with_capacity(256);
-    let mut maps:HashMap<char,i32> = HashMap::with_capacity(256);
-    let new_s:Vec<char> = s.chars().collect();
+// pub fn is_right(mapt:&HashMap<char, i32>, maps:&HashMap<char, i32>) -> String {
 
-    let ans:String = String::new();
-    let mut left = 0;
-    let mut count =  0;
-    let mut st_index = 0;
+// }
+
+pub fn min_window(s:String, t:String) -> String {
+    let mut need:HashMap<char, i32>  = HashMap::with_capacity(256);
+
 
     for ch in  t.chars() {
-        *mapt.entry(ch).or_insert(0) += 1;
+        *need.entry(ch).or_insert(0) += 1;
     }
 
-    for right in 0..new_s.len() {
-        *mapt.entry(new_s[right]).or_insert(0) += 1;
+    let mut window:HashMap<char,i32> = HashMap::with_capacity(256);
+    let chars:Vec<char> = s.chars().collect();
 
-        while right < new_s.len() {
-            
-        }   
+    let mut left = 0;
+    let mut count = 0;
+    let required = need.len();
 
+    let mut best_start = 0;
+    let mut best_len = std::usize::MAX;
+
+    for right in 0..chars.len() {
+        let ch = chars[right];
+
+        if need.contains_key(&ch) {
+            *window.entry(ch).or_insert(0) += 1;
+
+            if window[&ch] == need[&ch] 
+            {
+                count += 1;
+            }
+
+        }
+
+        while count == required {
+            let len = right - left + 1;
+
+            if len < best_len {
+                best_len = len;
+                best_start = left;
+            }
+
+            let left_char = chars[left];
+
+            if need.contains_key(&left_char) {
+                if window[&left_char] == need[&left_char] {
+                    count -= 1;
+                }
+                *window.get_mut(&left_char).unwrap() -= 1;
+            }
+            left += 1;
+        }
     }
         
+    let ans:String = chars[best_start..best_start + best_len].iter().collect();
 
+    if best_len == usize::MAX {
+    return String::new();
+} else {
     ans
+}
+    
 }
 
 fn main() {
     let s = String::from("ADOBECODEBANC");
     let t = String::from("ABC");
+
+    let ans = min_window(s, t);
+
+    println!("{}", ans);
+
 }
