97f6f3e5ff07d2806548ae1b07a0a880c360df8b
Roodius-hub <osmansaifi30@gmail.com>
2026-08-29 04:53:11 -0700

    adding

---
+++ src/company/424.rs
@@ -0,0 +1,19 @@
+
+
+
+pub fn character_replacement(s: String, k: i32) -> i32 {
+    let new_s:Vec<char> = s.chars().collect();
+    let n = new_s.len();
+
+    
+
+}
+
+
+
+
+fn main() {
+    let s = String::from("ABAB");
+    let ans = character_replacement(s, 2);
+    println!("{}",ans);
+}
