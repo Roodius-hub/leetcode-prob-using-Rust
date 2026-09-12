5ad62d626e828224cb959d97c8962053fcda5987
Roodius-hub <osmansaifi30@gmail.com>
2026-09-12 07:26:57 -0700

    replace string

--- src/company/424.rs
+++ src/company/424.rs
@@ -1,12 +1,35 @@
 
-
-
 pub fn character_replacement(s: String, k: i32) -> i32 {
-    let new_s:Vec<char> = s.chars().collect();
-    let n = new_s.len();
+    let bytes = s.as_bytes();
 
-    
 
+
+    let mut answer = 0;
+
+
+    for l in 0..bytes.len() {
+        // let index = (bytes[l] - b'A') as usize;
+        // freq[index] += 1;
+        for r in l+1..bytes.len() {
+            let mut freq = [0; 26];
+
+            for i in l..=r {
+
+                let index = (bytes[i] - b'A') as usize;
+                freq[index] += 1;
+            }
+
+            let max_freq = *freq.iter().max().unwrap();
+            let window_size = r - l + 1;
+            let replacement_needed = window_size - max_freq;
+ 
+            if replacement_needed as i32 <= k {
+                answer = answer.max(window_size);
+            }
+        }
+    }
+
+    answer as i32
 }
 
 
