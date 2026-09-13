2e0635b0a1dd5d5e35ead0a38c32c33017884031
Roodius-hub <osmansaifi30@gmail.com>
2026-09-13 03:22:39 -0700

    271

--- src/company/424.rs
+++ src/company/424.rs
@@ -1,32 +1,23 @@
 
 pub fn character_replacement(s: String, k: i32) -> i32 {
     let bytes = s.as_bytes();
-
-
-
     let mut answer = 0;
+    let mut freq = [0; 26];
+    let mut left = 0;
+    let mut max_freq = 0;
 
+    for right in 0..bytes.len() {
+        let index = (bytes[right] - b'A') as usize;
+        freq[index] += 1;
 
-    for l in 0..bytes.len() {
-        // let index = (bytes[l] - b'A') as usize;
-        // freq[index] += 1;
-        for r in l+1..bytes.len() {
-            let mut freq = [0; 26];
+        max_freq = max_freq.max(freq[index]);
 
-            for i in l..=r {
-
-                let index = (bytes[i] - b'A') as usize;
-                freq[index] += 1;
-            }
-
-            let max_freq = *freq.iter().max().unwrap();
-            let window_size = r - l + 1;
-            let replacement_needed = window_size - max_freq;
- 
-            if replacement_needed as i32 <= k {
-                answer = answer.max(window_size);
-            }
+        while (right - left + 1) as i32 - max_freq > k  {
+              let left_index = (bytes[left] - b'A') as usize;
+              freq[left_index] -= 1;
+              left += 1;
         }
+        answer = answer.max(right - left + 1)
     }
 
     answer as i32
